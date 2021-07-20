dd571d948b093b3b81025f48a41160c60093adb8
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-07-20 16:43:17 +0300

    chore(plugins): add vue3-editor

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue';
 
+import Vue3Editor from 'vue3-editor';
+
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 
 import '@/assets/styles/main.scss';
 
-createApp(App)
+const app = createApp(App);
+
+app
   .use(store)
   .use(router)
+  .use(Vue3Editor);
+
+app
   .mount('#app');
