374d92079445912396a2344dea1a1b0287abf3f3
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-07-21 15:37:42 +0300

    feat: add post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="home">
-    Home
+    <post :post="welcomeScreen" />
+    <post v-for="post of samplePosts" :key="post.id" :post="post" />
   </div>
 </template>
 
 <script>
+import Post from '@/components/Post.vue';
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      welcomeScreen: {
+        title: 'Welcome!',
+        content: 'Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!',
+        welcomeScreen: true,
+        photo: 'coding.jpg',
+      },
+      samplePosts: [
+        {
+          id: '1',
+          title: 'This is a Filler Title',
+          content: 'This is a filler post title!',
+          coverPhoto: 'beautiful-stories.jpg',
+        },
+        {
+          id: '2',
+          title: 'This is a Filler Title',
+          content: 'This is a filler post title!',
+          coverPhoto: 'designed-for-everyone.jpg',
+        },
+      ],
+    };
+  },
+  components: {
+    Post,
+  },
 };
 </script>
